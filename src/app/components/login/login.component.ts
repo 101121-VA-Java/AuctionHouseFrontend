fda177e7273214269893080229f5b8d19039ec8c
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-12-10 14:23:26 -0500

    login and logout works with backend api

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,22 +9,29 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
-  constructor(private Auth: AuthService) { 
-   
-  }
+  uname = "";
+  pw = "";
+
+  constructor(private Auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
   }
 
-  login(){
-    const username = (<HTMLInputElement>document.getElementById('username')).value;
-    const password = (<HTMLInputElement>document.getElementById('password')).value;
-
-    console.log(username,password);
-    this.Auth.userInfo(username, password).subscribe(data =>{
+  login(): void{
+    const uname = this.uname
+    const pw = this.pw
+    let data: any = {uname, pw};
+    data = JSON.stringify(data);
+    this.Auth.login(data).subscribe(res =>{
       if(HttpStatusCode.Ok){
-        //redirect to page
+        let role = Number.parseInt(this.Auth.token.split(":")[1]);
+        if(role === 1){
+          this.router.navigate(['/auctioneer'])
+        } else if(role === 2){
+          this.router.navigate(['/client'])
+        } else {
+          this.router.navigate(['/'])
+        };
       } else{
         console.log("not working");
       }
